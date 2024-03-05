029ea33f522d3bfaf3491631b1f406192e9cb940
Ronny Gabriel Matute Granizo <rgmatute91@gmail.com>
2024-03-05 13:19:11 -0500

    Ajustes para el modal de carga de fichero!

--- controllers/DriverController.php
+++ controllers/DriverController.php
@@ -15,7 +15,7 @@
 			$archivos = scandir($_ENV['REPOSITORY'] ?? 'store');
 			$response =  array();
 			foreach ($archivos as $archivo) {
-				if($archivo != "." && $archivo != ".." && $archivo != 'index.php') {
+				if($archivo != "." && $archivo != ".." && $archivo != 'index.php' && !strstr($archivo, 'eliminado')) {
 					array_push($response, ['name' => $archivo]);
 				}
 
@@ -38,6 +38,32 @@
 			} else {
 				$response = [ 'message' => "No se recibió ningún archivo.", 'suscess' => false ];
 			}
+
+			return response()->json($response);
+		}
+
+		public function renombrarFichero(Request $request) {
+			$directorio_destino = $_ENV['REPOSITORY'] ?? 'store';
+			$response =  array();
+
+			$fichero = $request->body('fichero');
+
+
+			$partes = explode('.', $archivo);
+
+			$nombre = implode('.', array_slice($partes, 0, -1)); // Obtiene el nombre del archivo sin la última parte
+			$extension = end($partes); // Obtiene la última parte como extensión
+
+			// $archivo_destino = $directorio_destino . '/' . basename($_FILES['file']['name']);
+
+			if (rename($directorio_destino.'/'.$fichero, $directorio_destino.'/eliminado-'.$fichero)) {
+				$response = [ 'message' => "El archivo se ha eliminado correctamente.", 'suscess' => true ];
+			} else {
+				$response = [ 'message' => "No se pudo eliminar el archivo..", 'suscess' => false ];
+			}
+			
+
+			// $response = [ 'message' => "No se recibió ningún archivo.", 'suscess' => false, 'fichero' => $fichero ];
 
 			return response()->json($response);
 		}
--- views/driver.php
+++ views/driver.php
@@ -6,6 +6,8 @@
     <div v-for="fichero in ficheros" style="z-index: -1!important;">
         <div class="text-center mb-2">
             <a class="badge badge-success" :href="armaRuta(fichero.name)" target="_blank"><h5>{{fichero.name}} <small>(descargar)</small> </h5></a>
+
+            <span class="btn btn-danger" @click="eliminaFichero(fichero.name)">Eliminar</span>
         </div>
     </div>
 
@@ -28,52 +30,50 @@
 
 
 <!-- Modal -->
-<div style="z-index: 1!important;" class="modal fade" :class="{show, 'd-block': active}"
-    id="exampleModal2"
-    tabindex="-1"
-    role="dialog"
+<div style="z-index: 1!important;" class="modal fade" :class="{show, 'd-block': active}" 
+    id="exampleModal2" 
+    tabindex="-1" 
+    role="dialog" 
     aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog " role="document" style="color: black">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Cargar fichero <span v-if="file">[{{file.name}}]</span></h5>
+        <h5 class="modal-title" id="exampleModalLabel">Cargar fichero <span v-if="file">[{{file.name}}]</span></h5> 
         <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="toggleModal">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-   <!--
+   <!--   
       <input v-if="!cargando" type="file" @change="onFileChange" multiple>
 -->
 
-<div class="bloque-drag-and-drog">
+<div class="bloque-drag-and-drog" v-if="!cargando">
         <!-- Área de arrastre de archivos -->
         <div class="drop-area" @drop.prevent="dropHandler" @dragover.prevent @click="openFileInput">
-            Arrastra y suelta archivos aquí
+            Arrastra y suelta archivos aquí, ó da click aquí
         </div>
         <!-- Input de tipo file oculto -->
         <input type="file" style="display: none" ref="fileInput" @change="handleFileInput">
         <!-- Lista de archivos seleccionados -->
-        <div class="table-responsive">
-            <table class="table table-striped table-sm table-dark " v-if="files.length > 0">
-                <thead>
-                    <tr>
-                        <th>Archivo</th>
-                        <th>Tamaño</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody class="text-center">
-                    <tr v-for="(file, index) in files" :key="index">
-                        <td class="truncate" :title="file.name">{{ file.name }}</td>
-                        <td>{{ fileSize(file.size) }}</td>
-                        <td>
-                            <button class="btn btn-danger btn-sm">Eliminar</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <table class="table table-striped table-sm table-dark " v-if="files.length > 0 && !cargando">
+            <head>
+                <tr>
+                    <th>Archivo</th>
+                    <th>Tamaño</th>
+                    <th></th>
+                </tr>
+            </head>
+            <body class="text-center">
+                <tr v-for="(file, index) in files" :key="index">
+                    <td>{{ file.name }}</td>
+                    <td>{{ fileSize(file.size) }}</td>
+                    <td>
+                        <button class="btn btn-danger btn-sm">Eliminar</button>
+                    </td>
+                </tr>
+            </body>
+        </table>
         <!--
         <ul>
             <li v-for="(file, index) in files" :key="index">
@@ -91,7 +91,7 @@
       </div>
 
       </div>
-      <div class="modal-footer" v-if="files.length > 0 /*!cargando*/">
+      <div class="modal-footer" v-if="files.length > 0 && !cargando">
         <button type="button" @click="uploadFile" class="btn btn-primary">Completar la carga de fichero</button>
       </div>
     </div>
@@ -138,6 +138,7 @@
                 setTimeout(() => (this.show = !this.show), 10);
             },
             onFileChange(event) {
+                console.log("44444444444444444444");
                 this.file = event.target.files[0];
             },
             uploadFile() {
@@ -185,8 +186,12 @@
                 },
 
             dropHandler(event) {
+                console.log("111111111111111111111111");
             // Evita el comportamiento predeterminado del navegador al soltar archivos
             event.preventDefault();
+
+            // esto si que se está guardando
+            //this.file = event.target.files[0];
 
             // Obtiene la lista de archivos soltados
             const droppedFiles = event.dataTransfer.files;
@@ -195,6 +200,10 @@
             this.files = Array.from(droppedFiles);
         },
         handleFileInput(event) {
+            console.log("2222222222222222222");
+            // esto si que se está guardando
+            this.file = event.target.files[0];
+
             // Obtiene la lista de archivos seleccionados a través del input file
             const selectedFiles = event.target.files;
 
@@ -202,6 +211,7 @@
             this.files = Array.from(selectedFiles);
         },
         openFileInput() {
+            console.log("333333333333333333333");
             // Abre el explorador de archivos al hacer clic en el área de arrastre
             this.$refs.fileInput.click();
         },
@@ -215,8 +225,56 @@
             const sizeInKB = Math.round(sizeInBytes / 1024); // Redondea al entero más cercano
 
             return `${sizeInKB} KB`;
-        }
-
-            },
+        },
+        eliminaFichero(nombreFichero) {
+            console.log(nombreFichero);
+            return Swal.fire({
+                title: "Ingrese la contraseña",
+                input: "password",
+                html: "Eliminar: <b class='text-danger'>" + nombreFichero + "</b>",
+                inputPlaceholder: "contraseña",
+                allowOutsideClick: false,
+                inputAttributes: {
+                    maxlength: "10",
+                    autocapitalize: "off",
+                    autocorrect: "off"
+                },
+                showCancelButton: true
+                })
+                .then((password)=> {
+                    if (password!==null && password.isConfirmed) {
+                        if(password.value === '123'){
+                            // return this.showToast('Se ha eliminado correctamente', 'success');
+                            return axios.post('/?c=driver&a=renombrarFichero', { 'fichero': nombreFichero })
+                            .then((respuesta) => {
+                                this.obtenerFicheros();
+                                return this.showToast('Se ha eliminado correctamente', 'success');
+                            });
+                        }else{
+                            return this.showToast('Contraseña incorrecta!', 'error');
+                        }
+                    }else {
+                        return this.showToast('Transacción cancelada!', 'warning');
+                    }
+                });
+        },
+            showToast(message, type = 'success') {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    }
+                    });
+                    return Toast.fire({
+                        icon: type,
+                        title: message
+                    });
+            }
+        },
     });
 </script>
